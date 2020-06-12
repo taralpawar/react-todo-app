067bab5ad2a6f3b3e029f99b287f0df8325d75c2
Taral Pawar <taralpawar@gmail.com>
2020-06-12 23:12:04 +0530

    Updates

---
+++ precache-manifest.2c9ebc2976b8bea5dea141381f7caea6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45f5d2ef2a7ef0c5ca00900606d5bc51",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "1c6ecb5ab62468465451",
+    "url": "/react-todo-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bb3f989e3371de82433d",
+    "url": "/react-todo-app/static/js/2.c038e16e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-todo-app/static/js/2.c038e16e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c6ecb5ab62468465451",
+    "url": "/react-todo-app/static/js/main.e5ba18ae.chunk.js"
+  },
+  {
+    "revision": "07e336b421de6988d4d5",
+    "url": "/react-todo-app/static/js/runtime-main.4408a370.js"
+  }
+]);
